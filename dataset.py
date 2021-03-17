e86d2a2aa6ee8005d4b078dbd994cbeec138d862
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2021-03-17 01:18:21 -0700

    Dataset bugfix, sort filenames

--- dataset.py
+++ dataset.py
@@ -24,11 +24,30 @@ class Dataset(torch.utils.data.Dataset):
     
     def __init__(self):
         dataset_path = join(os.curdir, dirname(__file__), 'orl-database-of-faces')
+
         # Sort by length first, then lexicographically
         dirnames = sorted([e for e in os.scandir(dataset_path) if e.is_dir()], key=lambda e: (len(e.name), e.name))
-        self.filenames = [(filename.path, label) for label, e in enumerate(dirnames) for filename in os.scandir(e) if filename.name.endswith(f'{os.extsep}pgm')]
+        self.filenames = [
+            (filename.path, label)
+            for label, e in enumerate(dirnames)
+            for filename in sorted(os.scandir(e), key=lambda e: (len(e.name), e.name))
+            if filename.name.endswith(f'{os.extsep}pgm')
+        ]
 
     def __getitem__(self, index):
-        # specify path relative to this script
-        label, entry = self.filenames[index]
+        # Specify path relative to this script
+        entry, label = self.filenames[index]
         return label, read_pgm(open(entry, 'rb'))
+
+def main():
+    from matplotlib import pyplot as plt
+    d = Dataset()
+    while True:
+        i = int(input('Enter an index: '))
+        label, f = d[i]
+        print(f'Displaying: {d.filenames[i][0]} with label {label}')
+        plt.imshow(f, plt.cm.gray)
+        plt.show()
+
+if __name__ == '__main__':
+    main()
