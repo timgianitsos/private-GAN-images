3aa6d463aa6e0d4e2ccaa513723de876ade004ba
Tim Gianitsos <timgianitsos@users.noreply.github.com>
2021-03-16 23:33:57 -0700

    Main and dataset

---
+++ dataset.py
@@ -0,0 +1,6 @@
+import torch.utils.data as data
+
+class Dataset(torch.utils.data.Dataset):
+    
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.utils.data
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
