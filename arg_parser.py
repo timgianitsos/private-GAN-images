875d6d368f0bff68ea3b817745d9424a7e36d29c
William Zhuk <williamzhuk@gmail.com>
2021-03-18 19:44:51 -0700

    cpu run fix

--- arg_parser.py
+++ arg_parser.py
@@ -22,25 +22,25 @@ class ArgParser(object):
         self.parser.add_argument('--name', type=str, default='debug', help='Experiment name prefix.')
         self.parser.add_argument('--seed', type=int, default=0, help='Random seed for reproducible outputs.')
 
-        self.parser.add_argument('--batch_size', type=int, default=2048, help='Batch size.')
+        self.parser.add_argument('--batch_size', type=int, default=64, help='Batch size.')
         self.parser.add_argument('--viz_batch_size', type=int, default=4, help='Visualization image batch size.')
 
-        self.parser.add_argument('--gpu_ids', type=str, default='0,1', help='Comma-separated list of GPU IDs. Use -1 for CPU.')
+        self.parser.add_argument('--gpu_ids', type=str, default='0', help='Comma-separated list of GPU IDs. Use -1 for CPU.')
         self.parser.add_argument('--num_workers', default=4, type=int, help='Number of threads for the DataLoader.')
 
         self.parser.add_argument('--save_dir', type=str, default=join(os.curdir, dirname(__file__), 'results'), help='Directory for results, prefix.')
         self.parser.add_argument('--num_visuals', type=str, default=4, help='Number of visual examples to show per batch on Tensorboard.')
 
         self.parser.add_argument('--num_epochs', type=int, default=1000, help='Number of epochs to train.')
-        self.parser.add_argument('--lr', type=float, default=2e-4, help='Learning rate.')
+        self.parser.add_argument('--lr', type=float, default=5e-5, help='Learning rate.')
         self.parser.add_argument('--step_train_discriminator', type=float, default=1, help='Train discriminator every x steps. Set x here.')
         self.parser.add_argument('--latent_dim', type=int, default=64, help='Dimension of z-vector that is passed into Generator.')
         
         self.parser.add_argument('--max_ckpts', type=int, default=15, help='Max ckpts to save.')
         self.parser.add_argument('--load_path', type=str, default=None, help='Load from a previous checkpoint.')
         
-        self.parser.add_argument('--steps_per_print', type=int, default=100, help='Steps taken for each print of logger')
-        self.parser.add_argument('--steps_per_visual', type=int, default=100, help='Steps for  each visual to be printed by logger in tb')
+        self.parser.add_argument('--steps_per_print', type=int, default=50, help='Steps taken for each print of logger')
+        self.parser.add_argument('--steps_per_visual', type=int, default=400, help='Steps for  each visual to be printed by logger in tb')
 
     def parse_args(self):
         args = self.parser.parse_args()
@@ -81,7 +81,7 @@ class ArgParser(object):
 
         args.gpu_ids = args_to_list(args.gpu_ids)
 
-        if len(args.gpu_ids) > 0 and torch.cuda.is_available():
+        if len(args.gpu_ids) > 0 and torch.cuda.is_available() and args.gpu_ids[0] >= 0:
             # Set default GPU for `tensor.to('cuda')`
             torch.cuda.set_device(args.gpu_ids[0])
             args.device = 'cuda'
